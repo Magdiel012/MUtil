43815392d53a4e46bb8d01c3e3ec90e3b9624f46
Magdiel012 <magdiel.a.music@gmail.com>
2022-09-27 14:46:56 -0400

    Clean up formatting

--- build/autogen.py
+++ build/autogen.py
@@ -13,13 +13,15 @@ def generate_sm_variants():
         base_text = re.sub("\n{2,}|\n\s{2,}\n", "\n", base_text)
 
     # Generate statemachine_play.zs.
-    play_message = """/*
+    play_message = """
+/*
 ┌──────────────────────────────────────────────────────────────────────────────┐
 │ These are play-scoped versions of the state machine types found in           │
 │ statemachines.zs. Please refer to the aforementioned file for documentation. │
 └──────────────────────────────────────────────────────────────────────────────┘
-*/"""
-    gen_text = "// AUTO-GENERATED\n\n" + play_message + "\n\n" + base_text
+*/
+"""
+    gen_text = "// AUTO-GENERATED\n" + play_message + base_text
     gen_text = re.sub("SMState", "SMStatePlay", gen_text)
     gen_text = re.sub("SMMachine", "SMMachinePlay", gen_text)
     gen_text = re.sub("SMTransition", "SMTransitionPlay", gen_text)
@@ -27,15 +29,16 @@ def generate_sm_variants():
     with open("../MBaseLib/statemachines/statemachine_play.zs", "w", encoding="utf-8") as f:
         f.write(gen_text)
 
-    # Generate statemachine_.zs.
-    ui_message = """/*
+    # Generate statemachine_ui.zs.
+    ui_message = """
+/*
 ┌──────────────────────────────────────────────────────────────────────────────┐
 │ These are UI-scoped versions of the state machine types found in             │
 │ statemachines.zs. Please refer to the aforementioned file for documentation. │
 └──────────────────────────────────────────────────────────────────────────────┘
-*/"""
-
-    gen_text = "// AUTO-GENERATED\n\n" + ui_message + "\n\n" + base_text
+*/
+"""
+    gen_text = "// AUTO-GENERATED\n" + ui_message + base_text
     gen_text = re.sub("SMState", "SMStateUI", gen_text)
     gen_text = re.sub("SMMachine", "SMMachineUI", gen_text)
     gen_text = re.sub("SMTransition", "SMTransitionUI", gen_text)
